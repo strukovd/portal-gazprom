33f25ab3b42e2f16c7f5e69ad919555cdc8a692e
strukovd <strukovd@users.noreply.github.com>
2025-12-23 09:50:22 +0000

    deploy: f649f7ceaf6796d9609fbf391125f62388975d64

---
+++ _nuxt/BRjPVNtu.js
@@ -0,0 +1,1 @@
+import{a0 as s}from"#entry";const t=s("app",{state:()=>({issues:[]}),actions:{}});export{t as u};
--- _nuxt/CBQEV4ZP.js
+++
@@ -1,1 +0,0 @@
-import{Y as s}from"#entry";const t=s("app",{state:()=>({issues:[]}),actions:{}});export{t as u};
